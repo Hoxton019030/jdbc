d3a485d0a04a360d0da8ee4aa7e2da0f615badb3
Hoxton <za546321@yahoo.com.tw>
2022-03-15 16:31:47 +0800

    2022-3-15

--- JDBC/src/m2_ConnectionDemo/GetConnectionDemo.java
+++ JDBC/src/m2_ConnectionDemo/GetConnectionDemo.java
@@ -8,12 +8,24 @@ public class GetConnectionDemo {
 		String url = "jdbc:sqlserver://localhost:1433;databasename=Northwind";
 		String userName = "Hoxton";
 		String password = "orz93098";
-		try {
-			Connection connection = DriverManager.getConnection(url, userName, password);
+
+		try (Connection connection = DriverManager.getConnection(url, userName, password)) {
+Statement createStatement = connection.createStatement();
+String selectProducts = "SELECT * FROM Products"; //目標陳述句
+ResultSet executeQuery = createStatement.executeQuery(selectProducts);
+
+while(executeQuery.next()) {
+	for(int i = 1 ; i<=10 ;i++) {
+		System.out.print(executeQuery.getString(i));
+	}
+	System.out.println();
+}
+
+
+
+
 			System.out.println("連線成功");
-			
-			connection.close();
-			
+
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
